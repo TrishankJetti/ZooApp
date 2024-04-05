11a4fb08549768ced770a67940cf56396a3d3fe6
Trishank Jetti <ac121430@avcol.school.nz>
2024-04-06 10:01:26 +1300

    Added a custom field to the Register page called First Name and Last Name.

--- ZooApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ZooApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,10 +70,22 @@ namespace ZooApp.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+
+            [DataType(DataType.Text)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+
+            [DataType(DataType.Text)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            /// 
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
---
+++ ZooApp/Data/UserClass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZooApp.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessage = "Please provide a valid First Name please")]
+        [PersonalData]
+        [Column(TypeName="nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage ="Please provide a valid First Name please")]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
