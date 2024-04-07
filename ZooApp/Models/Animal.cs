ed1de93e79cf431612c61c5f4f369c966ba3dbef
Trishank Jetti <ac121430@avcol.school.nz>
2024-04-07 18:49:53 +1200

    Made soem commetns on one of my tables

--- ZooApp/Models/Animal.cs
+++ ZooApp/Models/Animal.cs
@@ -3,14 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZooApp.Models
 {
-    public enum Sex
+    public enum Sex // The possible Genders for an animal are defied here.
     {
         Male,
         Female,
         Unidentified
     }
 
-    public enum DietType
+    public enum DietType // The possible Dietypes for an animal are defied here.
     {
         Carnivore,
         Omnivore,
@@ -20,42 +20,54 @@ namespace ZooApp.Models
 
     public class Animal
     {
+
+        //Foreign Key (Unique IDentifier for each record in table)
         [Key]
         public int AnimalId { get; set; }
 
+        //Name field of the Animal string length can around 100 characters long, can't have numbers
+        //or special characters and required field.
         [Required]
         [StringLength(100)]
         [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Name has to begin with a capital letter and must not include any special characters or numbers.")]
         public string Name { get; set; }
 
-
+        //Species is a required field, with the maxiumum of 100 characters length. CAn't have special characters or numbers.
         [Required (ErrorMessage = "You must specify a Species type to submit.")]
         [StringLength(100 , ErrorMessage ="Please enter a valid Species name.")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Species has to begin with capital letter and must not include any special characters or numbers.")]
         public string Species { get; set; }
 
+        //The Age is required field, and can span from 0 to 100 years.
         [Required]
         [Range(1, 100, ErrorMessage = "Please enter a valid age above 0, and less than 100")]
         public int Age { get; set; }
 
+        //Sex or Gender is an Enum, where there are predefined Values that users can select from a drop downlist 
+        //of values defined below.
         [Required]
         public Sex Sex { get; set; }
 
+        //The diet field is also an enum like the Sex field.
         [Required]
         public DietType? Diet { get; set; }
         
-       
+       // Foreign Keys
+       //This is the Employees that take care of the particular Animal
         public int EmployeeId { get; set; }
         [ForeignKey(nameof(EmployeeId))]
         [Required(ErrorMessage = "You must select an Employee to create Animal.")]
         public Employee Employee { get; set; }
 
-
+        //This is the Enclosures that houses each Animal.
         public int EnclosureId { get; set; }
         [ForeignKey(nameof(EnclosureId))]
         [Required(ErrorMessage = "You must select an Enclosure to assign Animal to.")]
         public Enclosure Enclosure { get; set; }
 
+
+        // This is a junction table the Animal's table is related with which brings both the animal and employees in a relationship
+        // but since SQL SERVER doesnt allow many to many relationships we must use a junction table inorder to make the relationship.
         public ICollection<AnimalEmployee> AnimalZooEmployees { get; set; }
     }
 }
